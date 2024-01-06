63ac80be873b0e02d4dc5d3adf2081ff82d155ba
ZvezdaNeycheva <138030249+ZvezdaNeycheva@users.noreply.github.com>
2024-01-06 07:17:20 +0200

    fixed html

--- Hackaton3/template/src/index.js
+++ Hackaton3/template/src/index.js
@@ -30,7 +30,52 @@ document.addEventListener('DOMContentLoaded', () => {
       toggleFavoriteStatus(+event.target.getAttribute(/* your correct data attribute here */));
     }
 
+  });import { HOME } from './common/constants.js';
+  import { toggleFavoriteStatus } from './events/favorites-events.js';
+  import { q } from './events/helpers.js';
+  import { loadPage, renderCategory, renderMovieDetails } from './events/navigation-events.js';
+  import { renderSearchItems } from './events/search-events.js';
+  
+  document.addEventListener('DOMContentLoaded', () => {
+  
+    // add global listener
+    document.addEventListener('click', event => {
+  
+      // nav events
+      if (event.target.classList.contains('nav-link')) {
+  
+        loadPage(event.target.getAttribute('data-page'));
+      }
+  
+      // show category events
+      if (event.target.classList.contains('btnn')) {
+        renderCategory(+event.target.getAttribute('id'));
+      }
+  
+      // show movie events
+      if (event.target.classList.contains('button-view-details')) {
+        renderMovieDetails(+event.target.getAttribute('id'));
+      }
+  
+      // toggle favorite event
+      if (event.target.classList.contains('favourites-btn')) {
+        toggleFavoriteStatus(+event.target.getAttribute('id'));
+      }
+  
+    });
+  
+    // search events
+    q('input#search').addEventListener('input', event => {
+      renderSearchItems(event.target.value);
+    });
+  
+    loadPage(HOME);
+  
+  
+  
   });
+  
+  
 
   // search events
   q('input#search').addEventListener('input', event => {
